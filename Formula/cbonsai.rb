9c274c45437289ec733ea323c146f29ae974eadb
Matthew Horwood <matt@horwood.biz>
2021-06-01 10:14:59 +0100

    bug: fix shar

--- Formula/cbonsai.rb
+++ Formula/cbonsai.rb
@@ -2,7 +2,7 @@ class Cbonsai < Formula
   desc "Console Bonsai is a bonsai tree generator, written in C using ncurses"
   homepage "https://github.com/mhzawadi/homebrew-cbonsai"
   url "https://github.com/mhzawadi/homebrew-cbonsai/archive/v1.2.0.tar.gz"
-  sha256 "7dba90fa9a922aa6e750f92ef704178bb3fb52c06752dac34a3718eb4554d2e1"
+  sha256 "a6984df8327141f7e6f24aca0d2194ec14f920ed16a5bc59b4490e9bdde69fba"
   license "GPL-3.0"
 
   def install
