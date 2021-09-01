0cee94a39cc57f22c3887558b5c425da51a0fcad
Matthew Horwood <matt@horwood.biz>
2021-09-01 15:12:08 +0100

    Update cbonsai.rb
    update

--- Formula/cbonsai.rb
+++ Formula/cbonsai.rb
@@ -1,8 +1,8 @@
 class Cbonsai < Formula
   desc "Console Bonsai is a bonsai tree generator, written in C using ncurses"
   homepage "https://github.com/mhzawadi/homebrew-cbonsai"
-  url "https://github.com/mhzawadi/homebrew-cbonsai/archive/v1.2.0.tar.gz"
-  sha256 "a6984df8327141f7e6f24aca0d2194ec14f920ed16a5bc59b4490e9bdde69fba"
+  url "https://github.com/mhzawadi/homebrew-cbonsai/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "0ccc4c0c65133cfd61ffc2403596a44437d60724034945d829702b6050da2c95"
   license "GPL-3.0"
 
   def install
