985bb86f26cc85851f912e3197a58a161aff0e08
Mattheworwood <matt.horwood@mayden.co.uk>
2021-09-01 15:39:02 +0100

    mho/fix build bug: set enviroment

--- Formula/cbonsai.rb
+++ Formula/cbonsai.rb
@@ -4,9 +4,11 @@ class Cbonsai < Formula
   url "https://github.com/mhzawadi/homebrew-cbonsai/archive/refs/tags/v1.3.1.tar.gz"
   sha256 "0ccc4c0c65133cfd61ffc2403596a44437d60724034945d829702b6050da2c95"
   license "GPL-3.0"
+  depends_on "pkg-config"
+  depends_on "ncurses"
 
   def install
-    system "export PKG_CONFIG_PATH=\"$PKG_CONFIG_PATH:/usr/local/Cellar/ncurses/6.2/lib/pkgconfig/\""
+    ENV["PKG_CONFIG_PATH"] = "$PKG_CONFIG_PATH:/usr/local/Cellar/ncurses/6.2/lib/pkgconfig/"
     system "make"
   end
 
