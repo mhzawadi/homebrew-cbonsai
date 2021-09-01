47b1fc8e7e4a34abc95f5d8c2fe9eaf724cebfd0
Matthew Horwood <matt@horwood.biz>
2021-09-01 15:48:11 +0100

    mho/fix build
    version bump

--- Formula/cbonsai.rb
+++ Formula/cbonsai.rb
@@ -1,8 +1,8 @@
 class Cbonsai < Formula
   desc "Console Bonsai is a bonsai tree generator, written in C using ncurses"
   homepage "https://github.com/mhzawadi/homebrew-cbonsai"
-  url "https://github.com/mhzawadi/homebrew-cbonsai/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 "0ccc4c0c65133cfd61ffc2403596a44437d60724034945d829702b6050da2c95"
+  url "https://github.com/mhzawadi/homebrew-cbonsai/archive/refs/tags/v1.3.2.tar.gz"
+  sha256 "79139324a9af73aa4c692398f70f156c1cd4ab0bf2660f927907396ad4247f52"
   license "GPL-3.0"
   depends_on "pkg-config"
   depends_on "ncurses"
