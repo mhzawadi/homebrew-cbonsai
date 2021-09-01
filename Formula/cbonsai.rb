0723afa108f8639e93392a921abca1e65bcbc482
Matthew Horwood <matt@horwood.biz>
2021-09-01 16:20:00 +0100

    Update cbonsai.rb
    reorder

--- Formula/cbonsai.rb
+++ Formula/cbonsai.rb
@@ -4,8 +4,8 @@ class Cbonsai < Formula
   url "https://github.com/mhzawadi/homebrew-cbonsai/archive/refs/tags/v1.3.2.tar.gz"
   sha256 "79139324a9af73aa4c692398f70f156c1cd4ab0bf2660f927907396ad4247f52"
   license "GPL-3.0"
-  depends_on "pkg-config"
   depends_on "ncurses"
+  depends_on "pkg-config"
 
   def install
     ENV["PKG_CONFIG_PATH"] = "$PKG_CONFIG_PATH:/usr/local/Cellar/ncurses/6.2/lib/pkgconfig/"
