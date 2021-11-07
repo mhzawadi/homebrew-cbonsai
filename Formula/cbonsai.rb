3b11f396852e336f343f6fd475b5bb7c9c2822bd
Matthew Horwood <matt@horwood.biz>
2021-11-07 08:17:14 +0000

    MHO/bugs chor: add new file

--- Formula/cbonsai.rb
+++ Formula/cbonsai.rb
@@ -2,7 +2,7 @@ class Cbonsai < Formula
   desc "Console Bonsai is a bonsai tree generator, written in C using ncurses"
   homepage "https://github.com/mhzawadi/homebrew-cbonsai"
   url "https://github.com/mhzawadi/homebrew-cbonsai/archive/refs/tags/v1.3.3.tar.gz"
-  sha256 "0eb83c279690b482cdaf19f8eda1d302f52cf5038c942b04625adeca90454797"
+  sha256 "74ff67f65d399ba22cca9cdc9e4c78e3113a5c61c75b23f8d214d5df6a3a06f3"
   license "GPL-3.0"
   depends_on "ncurses"
   depends_on "pkg-config"
